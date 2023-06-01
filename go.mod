d69262958eec6dc3cf82f110673011c683fb63f5
vardhan.kottamasu <vardhan.kottamasu@npci.org.in>
2023-06-02 01:33:52 +0530

    Structures have been modified with json metadata for marshalling and few more functions have been written

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-task
 go 1.20
 
 require (
+	github.com/gogo/protobuf v1.3.2
 	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/onsi/ginkgo v1.7.0 // indirect
 	github.com/onsi/gomega v1.4.3 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
+const (
+	DB_PATH = "/home/vardhan.kottamasu@npci.org.in/.leveldb/demodb"
+)
+
 type Block struct {
-	blockNumber       int
-	transactionList   []txn
-	previousBlockHash string
+	blockNumber       int         `json:"blocknumber"`
+	transactionList   []txn       `json:transactions`
+	status            blockStatus `json:blockstatus`
+	previousBlockHash string      `json:previousblockhash`
+	blockHash         string      `json:blockhash`
 }
 
 type blockStatus string
@@ -26,11 +34,11 @@ const (
 )
 
 type txn struct {
-	txnID     int         `json:""`
-	valid     bool        `json:""`
-	timeStamp time.Time   `json:""`
-	status    blockStatus `json:""`
-	value     string      ``
+	txnID     int
+	timeStamp time.Time
+	valid     bool		`json:"valid"`
+	value     string	`json:"value"`
+	version   string	`json:"version"`
 }
 
 type iBlock interface {
@@ -38,32 +46,52 @@ type iBlock interface {
 	updateBlock(transaction txn)
 }
 
-func (b Block) pushValidTransaction(transaction txn) {
-	if transaction.status == COMMITTED {
-		db, err := leveldb.OpenFile("/home/vardhan.kottamasu@npci.org.in/.leveldb/demodb", nil)
-		if err == nil {
-			db.Put([]byte("key3"), []byte(transaction), nil)
+func isTransactionValid(transaction txn) {
+	if transaction.valid == true {
+		block := &Block{}
+		block.pushTransaction(transaction)
+		writeBlockToLedger(*block)
+	}
+}
+
+func (b Block) pushTransaction(transaction txn) {
+	//TODO: This count variable has to be fixed, as it resets everytimr code reruns
+	var count int = 0
+	if transaction.valid == true {
+		db, err := leveldb.OpenFile(DB_PATH, nil)
+		if err != nil {
+			fmt.Println("Error. DB can't be created")
+		} else {
+			count++
+			db.Put([]byte("key"+string(rune(count))), []byte(transaction.value), nil)
 		}
 		defer db.Close()
 	}
+}
 
+func (b Block) updateBlock(block Block) {
+	block.status = COMMITTED
 }
 
 func readDB(key string) {
-	db, err := leveldb.OpenFile("/home/vardhan.kottamasu@npci.org.in/.leveldb/demodb", nil)
+	db, err := leveldb.OpenFile(DB_PATH, nil)
 	if err == nil {
-		data, err := db.Get([]byte("key3"), nil)
+		data, err := db.Get([]byte(key), nil)
 		if err == nil {
 			fmt.Printf("%v", data)
 		}
 	}
 }
 
+func writeBlockToLedger(block Block) {
+	ledger,err := os.OpenFile("ledger.json",os.O_APPEND|os.O_CREATE,0777)
+	if(err!=nil){
+		fmt.Println("Error creating ledger file")
+	}else{
+		block.blockHash = md5.Sum([]byte("sample"))
+	}
+}
+
 func main() {
-	s := "strong"
-	str := md5.Sum([]byte(s))
-	block := Block{}
-	block.pushValidTransaction(s)
-	fmt.Printf("%x", str)
-	readDB("key3")
+// Once errors with all the functions are cleared, you can start implementing the functions here
 }
